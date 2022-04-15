b981a9c3e953475d8787984058df62dd5a7d2af8
Olesya Schukina <olesyaschu@gmail.com>
2022-04-16 03:21:13 +0600

    Delete functions for WETH and DAI swaps

--- contracts/Balancer.sol
+++ contracts/Balancer.sol
@@ -8,56 +8,30 @@ import "@balancer-labs/v2-vault/contracts/interfaces/IBasePool.sol";
 
 contract Balancer {
     IVault private vault = IVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
-    // IBasePool private constant bPool = IBasePool(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
-    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    IERC20 private daiToken = IERC20(DAI);
-    IERC20 private wethToken = IERC20(WETH);
 
-    function balanceDai() public view returns (uint) {
-        return daiToken.balanceOf(address(this));
+    function balanceOfToken(address _address) public view returns (uint) {
+        IERC20 token = IERC20(_address);
+        return token.balanceOf(address(this));
     }
 
-    function balanceWeth() public view returns (uint) {
-        return wethToken.balanceOf(address(this));
-    }
-
-    function swapWethForDai(bytes32 _poolId, uint256 _amount, uint256 _limit) public {
-        // daiToken.approve(address(vault), type(uint).max);
+    function swapTwoTokens(bytes32 _poolId, address _addressIn, address _addressOut, uint256 _amount, uint256 _limit) public {
         require(_amount >= 1000000000000, 'The amount must be greater or equal than 1000000000000');
-        wethToken.approve(address(vault), _amount);
+        IERC20 token = IERC20(_addressIn);
+        token.approve(address(vault), _amount);
         IVault.SingleSwap memory singleSwap = IVault.SingleSwap({
             poolId: _poolId,
             kind: IVault.SwapKind.GIVEN_IN,
-            assetIn: IAsset(WETH), // send weth
-            assetOut: IAsset(DAI), // get dai
+            assetIn: IAsset(_addressIn),
+            assetOut: IAsset(_addressOut),
             amount: _amount, // > 1000000000000
             userData: ''
         });
-        swap(singleSwap, _limit);
-    }
-
-    function swapDaiForWeth(bytes32 _poolId, uint256 _amount, uint256 _limit) public {
-        require(_amount >= 1000000000000, 'The amount must be greater or equal than 1000000000000');
-        daiToken.approve(address(vault), _amount);
-        IVault.SingleSwap memory singleSwap = IVault.SingleSwap({
-            poolId: _poolId,
-            kind: IVault.SwapKind.GIVEN_IN,
-            assetIn: IAsset(DAI), // send dai
-            assetOut: IAsset(WETH), // get weth
-            amount: _amount, // > 1000000000000
-            userData: ''
-        });
-        swap(singleSwap, _limit);
-    }
-
-    function swap(IVault.SingleSwap memory _singleSwap, uint256 _limit) public {
         IVault.FundManagement memory fundManagement = IVault.FundManagement({
             sender: address(this),
             fromInternalBalance: false,
             recipient: payable(address(this)),
             toInternalBalance: false
         });
-        vault.swap(_singleSwap, fundManagement, _limit, block.timestamp);
+        vault.swap(singleSwap, fundManagement, _limit, block.timestamp);
     }
 }
